64a05b73fd6c6378205c5668848d84a796830c59
AnkurMathur14 <ank.mathur14@gmail.com>
2022-08-26 03:07:38 +0530

    Trie data structure implementation

--- node.py
+++ node.py
@@ -49,3 +49,18 @@ class TreeNode(Node):
     def __str__(self):
         return "{} {}".format(self.data, self.left, self.right)
 
+
+class TrieNode:
+    def __init__(self):
+        self.is_terminated = False
+        self.children = [None] * 26
+
+    def __repr__(self):
+        return "{}".format(TrieNode.__class__.__name__)
+
+    def __str__(self):
+        return "{} {}".format(self.children, self.is_terminated)
+
+    def __del__(self):
+        for child in self.children:
+            del child
---
+++ trie.py
@@ -0,0 +1,88 @@
+"""
+Trie data structure implementation.
+Trie is also known as suffix tree or digital tree.
+Trie is used to store and search english words very efficiently.
+Search complexity: O(k) where k is the length of the word
+"""
+
+from node import TrieNode
+
+
+class Trie:
+    def __init__(self, word=None):
+        self.root = TrieNode()
+        if word:
+            self.create_trie(word)
+
+    def __repr__(self):
+        return "{}()".format(Trie.__class__.__name__)
+
+    def __str__(self):
+        pass
+
+    def __del__(self):
+        del self.root
+
+    def create_trie(self, word):
+        self.insert(word)
+
+    def insert(self, word):
+        curr = self.root
+        for ch in word:
+            if not curr.children[ord(ch) - ord('A')]:
+                curr.children[ord(ch) - ord('A')] = TrieNode()
+            curr = curr.children[ord(ch) - ord('A')]
+        curr.is_terminated = True
+
+    def search(self, word):
+        curr = self.root
+        for ch in word:
+            if not curr.children[ord(ch) - ord('A')]:
+                return False
+            curr = curr.children[ord(ch) - ord('A')]
+        return curr.is_terminated
+
+    @staticmethod
+    def __search_with_wild_char(node, word, pos):
+        if pos >= len(word):
+            return node.is_terminated
+
+        if word[pos] != "*":
+            node = node.children[ord(word[pos]) - ord('A')]
+            if not node:
+                return False
+            return Trie.__search_with_wild_char(node, word, pos + 1)
+
+        for i in range(26):
+            if node.children[i] and Trie.__search_with_wild_char(node.children[i], word, pos + 1):
+                return True
+        return False
+
+    def search_with_wild_char(self, word):
+        return Trie.__search_with_wild_char(self.root, word, 0)
+
+    def starts_with(self, word):
+        curr = self.root
+        for ch in word:
+            if not curr.children[ord(ch) - ord('A')]:
+                return False
+            curr = curr.children[ord(ch) - ord('A')]
+        return True
+
+
+def main():
+    t = Trie("START")
+
+    words = ["ARE", "AS", "DO", "DOT", "NEW", "NEWS", "NO", "NOT", "JUMPER"]
+    for word in words:
+        t.insert(word)
+
+    print(t.search("START"))  # True
+    print(t.search("JUMP"))  # False
+    print(t.starts_with("JUMP"))  # True
+    print(t.search_with_wild_char("JU*P*R"))  # True
+    print(t.search_with_wild_char("DOT*"))  # False
+
+
+if __name__ == '__main__':
+    main()
